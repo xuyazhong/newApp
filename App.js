6e6a28f4d6b4d25fa12d66d2c61caded10d85891
xuyazhong <xuyazhong@foxmail.com>
2018-03-05 11:52:16 +0800

    001

--- App.js
+++ App.js
@@ -9,7 +9,9 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  AsyncStorage
+  
 } from 'react-native';
 
 const DEFAULT_URL = 'http://www.weather.com.cn/data/cityinfo/101010100.html'
@@ -28,18 +30,60 @@ export default class App extends Component<Props> {
     
   }
 
-  network() {
+  async network() {
     // 网络请求
+    fetch(DEFAULT_URL).then((result)=>{return result.json()}).then((result)=>{
+       
+      const result = console.log(result)    
+    })
+
   }
 
   sort() {
     // 排序
-    
+    let arr= [4,10,8,3,5,1,2,6,7,9];
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j>arr.length;j++){
+            if(arr[i]>arr[j]){
+                // [arr[i],arr[j]]=[arr[j],arr[i]]
+                let tmp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=tmp;
+            }
+        }
+    }
+    const sort1=()=>{
+      let leftArr=[],rightArr=[];
+      for(let i=0;i<arr.length;i++){
+          if(arr[i]<arr[0]){
+              leftArr.push(arr[i])
+          }else{
+            rightArr.push(arr[i])
+          }
+      }
+        return [].concat(sort1(leftArr),arr[0],sort1(rightArr))
+    }
   }
 
   fuzzbuzz() {
     // 被3 整除输出"fuzz" 被5整除输出"buzz" 同时被3和5整除输出"fuzzbuzz"
-    
+    const test=()=>{
+      for(let i=0;i<100;i++){
+        if(i%3==0&&i%5==0){
+            console.log("fuzzbuzz")
+            continue;
+        }
+        if(i%3==0){
+            console.log("fuzz")
+            continue;
+        }else if(i%5==0){
+            console.log("buzz")
+            continue;
+        }
+      }
+    }
+
+
   }
 
   render() {
